598232c88dd3dd041fc74c8e799c98cf6dd03259
Şenol Emre Koçtaş <senolemrekoctas@gmail.com>
2025-09-10 17:06:15 +0300

    pages

--- client/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-
-export default defineConfig({
-  base: '/summarizerwebversion/',
-  plugins: [react()],
-  server: { port: 5173 },
-})
